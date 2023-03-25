2317faa06b67a534de3cd55cd78967dd6500dccc
VeeraSatyaSai <veerasatyasais@gmail.com>
2023-03-25 18:14:53 +0530

    "file opening done"

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/<filename>")
+def index(filename="file1"):
+  lines=[]
+  if (filename=="file2" or filename=="file4"):
+  
+    with open(".//"+filename+".txt","r",encoding='utf-16') as file:
+       for line in file:
+         lines.append(line)
+    return render_template('index.html',filename=lines)
+  
+  elif(filename=="file1" or filename=="file3"):
+    with open(".//"+filename+".txt","r",encoding='utf-8') as file:
+       for line in file:
+         lines.append(line)
+    return render_template('index.html',filename=lines)
+      
 
 if __name__ == "__main__":
     app.run(debug=True)

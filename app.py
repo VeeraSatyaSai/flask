94e5bfd6aa6fe445562457da393d6085f03f6c4c
VeeraSatyaSai <veerasatyasais@gmail.com>
2023-03-26 20:32:06 +0530

    "Added useful comments "

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask,render_template,request
+
 app=Flask(__name__)
 @app.route("/")
 @app.route("/<string:filename>")
+#Below function will run for both of the above url paths
 def index(filename="file1"):
  try:
- 
+   #Extracting values from query parameters
+   #If query parameters were not present in url it will return None 
    start=(request.args.get('start'))
    end=(request.args.get('end'))
-  
-  
-
    lines=[]
    if (filename=="file2" or filename=="file4"):
-    
-  
      with open(".//"+filename+".txt","r",encoding='utf-16') as file:
        for line in file:
+         #adding each line to list
          lines.append(line)
        if start==None:
           start=0
@@ -32,7 +31,7 @@ def index(filename="file1"):
           return render_template('differenterror.html',error="length of file exceeded")
        elif start<0 or end <0:
           return render_template('differenterror.html',error="Invalid Query Parameters")         
-        
+     #passing the list of lines to the filename variable in the index.html  
      return render_template('index.html',filename=lines[start:end])
 
     
@@ -40,6 +39,7 @@ def index(filename="file1"):
    elif(filename=="file1" or filename=="file3"):
     with open(".//"+filename+".txt","r",encoding='utf-8') as file:
        for line in file:
+        #adding each line to list
          lines.append(line)
        if start==None:
           start=0
@@ -56,17 +56,19 @@ def index(filename="file1"):
           return render_template('differenterror.html',error="length of file exceeded")
        elif start<0 or end <0:
          return render_template('differenterror.html',error="Invalid Query Parameters")
- 
+    #passing the list of lines to the filename variable in the index.html  
     return render_template('index.html',filename=lines[start:end])
    else:
+    #if wrong filename was given in the url parameters the differenterror.html will be rendered
     return  render_template('differenterror.html',error="File doesn't exist")
+    #if error occurs in try block then except block will be executed
  except Exception as e:
-    
+    #if query parameters given were invalid then the except block will run and this return html will be rendered
     return render_template('differenterror.html',error="Invalid Query Parameters")
  
  
  
-
+#if url path given was wrong this function will run
 @app.errorhandler(404)
 def someError(e):
     return render_template('error.html')
